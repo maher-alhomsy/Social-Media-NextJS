43af91242ef1919e1c6b916066cc1cf923e79d34
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-08-02 04:21:55 +0300

    create and add a menu-bar for small and large screens

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import { validateRequest } from "../../../auth";
 import SessionProvider from "@/context/session-provider";
+import MenuBar from "@/components/MenuBar";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
@@ -13,7 +14,12 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <SessionProvider value={session}>
       <div className="min-h-screen flex flex-col">
         <Navbar />
-        <div className="max-w-7xl mx-auto p-5">{children}</div>
+        <div className="max-w-7xl mx-auto p-5 flex w-full grow gap-5">
+          <MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
+          {children}
+        </div>
+
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
