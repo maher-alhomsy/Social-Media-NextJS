f22882a212fff345c7b5c13075a14c071b69b3e1
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-08-10 19:21:34 +0300

    create Post component, show posts inside Home page

---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import { PostData } from "@/lib/types";
+import UserAvatar from "../UserAvatar";
+import { formatRelativeData } from "@/lib/utils";
+
+type Props = {
+  post: PostData;
+};
+
+const Post = ({ post }: Props) => {
+  return (
+    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <UserAvatar avatarUrl={post.user.avatarUrl} />
+        </Link>
+
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          >
+            {formatRelativeData(post.createdAt)}
+          </Link>
+        </div>
+      </div>
+
+      <div className="whitespace-pre-line break-words">{post.content}</div>
+    </article>
+  );
+};
+
+export default Post;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatRelativeData(from: Date) {
+  const currentData = new Date();
+
+  if (currentData.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true });
+  } else {
+    if (currentData.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM d");
+    } else {
+      return formatDate(from, "MMM d, yyyy");
+    }
+  }
 }
